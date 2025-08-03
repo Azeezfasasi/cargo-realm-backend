c35e84951b887cb207ee97c0dfe41ca46fdce651
azeezfasasi <fas.azeez@gmail.com>
2025-08-03 16:41:06 +0100

    cool

--- routes/contactFormRoutes.js
+++ routes/contactFormRoutes.js
@@ -3,12 +3,22 @@ const router = express.Router();
 const contactFormController = require('../controllers/contactFormController');
 const { authenticate, authorize } = require('../middleware/auth');
 
+// Route to submit a contact form POST - /api/contact
 router.post('/', contactFormController.submitContactForm);
-router.get('/', authenticate, authorize('admin','pastor'), contactFormController.getAllContactForms);
-router.put('/:id', authenticate, authorize('admin','pastor'), contactFormController.editContactForm);
-router.delete('/:id', authenticate, authorize('admin','pastor'), contactFormController.deleteContactForm);
 
-// Route to reply to a contact form submission
-router.post('/:id/reply', authenticate, authorize('admin', 'pastor'), contactFormController.replyToContactForm);
+// Route to get all contact forms GET - /api/contact
+router.get('/', authenticate, authorize('admin', 'employee'), contactFormController.getAllContactForms);
+
+// Route to get a single contact form GET - /api/contact/:id
+router.get('/:id', authenticate, authorize('admin', 'employee'), contactFormController.getContactForm);
+
+// Route to edit a contact form PUT - /api/contact/:id
+router.put('/:id', authenticate, authorize('admin', 'employee'), contactFormController.editContactForm);
+
+// Route to delete a contact form DELETE - /api/contact/:id
+router.delete('/:id', authenticate, authorize('admin'), contactFormController.deleteContactForm);
+
+// Route to reply to a contact form POST - /api/contact/:id/reply
+router.post('/:id/reply', authenticate, authorize('admin', 'employee'), contactFormController.replyToContactForm);
 
 module.exports = router;
