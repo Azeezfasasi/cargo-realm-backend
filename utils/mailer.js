14bf229f8141f6b0786b8b63aea7289cef1ef85c
Azeezfasasi <fas.azeez@gmail.com>
2025-08-05 16:22:52 +0100

    email setup

--- utils/mailer.js
+++ utils/mailer.js
@@ -19,3 +19,28 @@ const sendMail = async (to, subject, html) => {
 };
 
 module.exports = sendMail;
+
+
+// const nodemailer = require('nodemailer');
+
+// const transporter = nodemailer.createTransport({
+//   host: 'smtp.zoho.com',
+//   port: 465,
+//   secure: true, // true for port 465, false for 587
+//   auth: {
+//     user: process.env.ZOHO_EMAIL_USER, // e.g., 'yourname@yourdomain.com'
+//     pass: process.env.ZOHO_EMAIL_PASS,
+//   },
+// });
+
+// const sendMail = async (to, subject, html) => {
+//   const mailOptions = {
+//     from: `"Cargo Realm and Logistics" <${process.env.ZOHO_EMAIL_USER}>`, // include a name
+//     to,
+//     subject,
+//     html,
+//   };
+//   return transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendMail;
