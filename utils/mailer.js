c0495cf0ac78aab2094621c3adc4d2018dd81145
azeezfasasi <fas.azeez@gmail.com>
2025-08-05 17:30:22 +0100

    updated email sender

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,41 +1,16 @@
-const nodemailer = require('nodemailer');
-
-const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
-
-const sendMail = async (to, subject, html) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  };
-  return transporter.sendMail(mailOptions);
-};
-
-module.exports = sendMail;
-
-
 // const nodemailer = require('nodemailer');
 
 // const transporter = nodemailer.createTransport({
-//   host: 'smtp.zoho.com',
-//   port: 465,
-//   secure: true, // true for port 465, false for 587
+//   service: 'gmail',
 //   auth: {
-//     user: process.env.ZOHO_EMAIL_USER, // e.g., 'yourname@yourdomain.com'
-//     pass: process.env.ZOHO_EMAIL_PASS,
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
 //   },
 // });
 
 // const sendMail = async (to, subject, html) => {
 //   const mailOptions = {
-//     from: `"Cargo Realm and Logistics" <${process.env.ZOHO_EMAIL_USER}>`, // include a name
+//     from: process.env.EMAIL_USER,
 //     to,
 //     subject,
 //     html,
@@ -44,3 +19,28 @@ module.exports = sendMail;
 // };
 
 // module.exports = sendMail;
+
+
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.zoho.com',
+  port: 465,
+  secure: true, // true for port 465, false for 587
+  auth: {
+    user: process.env.ZOHO_EMAIL_USER, // e.g., 'yourname@yourdomain.com'
+    pass: process.env.ZOHO_EMAIL_PASS,
+  },
+});
+
+const sendMail = async (to, subject, html) => {
+  const mailOptions = {
+    from: `"Cargo Realm and Logistics" <${process.env.ZOHO_EMAIL_USER}>`, // include a name
+    to,
+    subject,
+    html,
+  };
+  return transporter.sendMail(mailOptions);
+};
+
+module.exports = sendMail;
