a0e5a91664e6006f92d1fe9e7107ab193dab04a2
Azeezfasasi <fas.azeez@gmail.com>
2026-01-10 15:29:59 +0100

    updated

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(cors({
     'https://cargo-realm-logistics.netlify.app',
     'https://cargorealmandlogistics.com',
     'http://cargorealmandlogistics.com',
+    'https://cargo-realm-logistics.vercel.app',
     'http://localhost:5173'
   ], // Your frontend URLs
   credentials: true, // Allow cookies/auth headers to be sent
